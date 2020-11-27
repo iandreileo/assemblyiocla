c1f57779ad5fc03153ed1ede351c0faf0134791c
sKzRO <pinkkw0w@gmail.com>
2020-11-27 17:15:17 +0200

    incercare strstr

--- my_strstr.asm
+++ my_strstr.asm
@@ -1,5 +1,6 @@
 %include "io.mac"
-
+section .data
+    iterator DD 0
 section .text
     global my_strstr
     extern printf
@@ -18,6 +19,40 @@ my_strstr:
     ;; DO NOT MODIFY
 
     ;; TO DO: Implement my_strstr
+
+    iterateThroughHaystack:
+        mov dword [iterator], 1
+        iterate:
+            xor eax, eax
+            ;punem in eax iteratorul
+            mov eax, [iterator]
+            ;punem in ah caracteru curent
+            ;eliberam edx-ul punandu l in stiva
+            push edx
+            ;mutam caracterul in edx
+            mov edx, [esi + eax]
+
+            ;eliberam ecx-ul in stiva
+            push ecx
+            ;mutam primul caracter din needle in ecx
+            mov ecx, [ebx + 0]
+
+            cmp edx, ecx
+
+            je isEqual
+            jne notEqual
+
+            isEqual:
+                PRINTF32 `%c aaa\x0`, ecx
+
+            notEqual:
+                ;urmatorul caracter
+                inc dword [iterator]
+                PRINTF32 `%d aaa\x0`, [iterator]
+            
+            ; PRINTF32 `%c \x0`, edx
+            pop edx
+            pop ecx
 
     ;; DO NOT MODIFY
     popa
--- vigenere.asm
+++ vigenere.asm
@@ -1,5 +1,6 @@
 %include "io.mac"
-
+section .data
+    iterator DD 0
 section .text
     global vigenere
     extern printf
@@ -18,6 +19,36 @@ vigenere:
     ;; DO NOT MODIFY
 
     ;; TODO: Implement the Vigenere cipher
+    xor ebx, ebx
+    keyToExtendedKey:
+        PRINTF32 `Equal %s    %s\x0`, esi, edi
+
+        ;iteram prin plaintext
+        xor eax, eax
+        mov al, byte [edx + ebx]
+        ;comparam al cu A
+        cmp al, 0x41
+        ; daca e mai mic sarim la urmatorul caracter              
+        jl next_char
+        ;comparam al cu Z               
+        cmp al, 0x5A               
+        ;daca al e intre A si Z am gasit o litera
+        jle found_letter           
+        ;compara al cu a
+        cmp al, 0x61               
+        ;sarim la urmatorul caracter
+        jl next_char
+        ;comparam al cu z               
+        cmp al, 0x7A               
+        ;daca e peste Z sarim la urmatorul caracter
+        jg next_char              
+        ;daca nu inseamna ca e intre a si z
+
+
+        found_letter:
+            ;prelucram litera
+        
+        next_char:
 
     ;; DO NOT MODIFY
     popa
